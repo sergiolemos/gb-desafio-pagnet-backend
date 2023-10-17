fbb772014b643ddc412c3c4fc816049ca5dd5935
Sérgio Lemos <contato@sergiolemos.com>
2023-10-16 21:56:17 -0300

    primeiro commit

---
+++ src/main/java/com/sergiolemos/backend/domain/Transacao.java
@@ -0,0 +1,66 @@
+package com.sergiolemos.backend.domain;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public record Transacao(
+    Long id,
+    Integer tipo,
+    Date data,
+    BigDecimal valor,
+    Long cpf,
+    String cartao,
+    Time hora,
+    String donoDaLoja,
+    String nomeDaLoja) {
+
+    public Transacao withValor(BigDecimal valor){
+        return new Transacao(
+                id,
+                tipo,
+                data,
+                valor,
+                cpf,
+                cartao,
+                hora,
+                donoDaLoja,
+                nomeDaLoja);
+    }
+
+    public Transacao wihtData(String data) throws ParseException {
+        var dateFormat = new SimpleDateFormat("yyyyMMdd");
+        var date = dateFormat.parse(data);
+
+        return new Transacao(
+                id,
+                tipo,
+                new Date(date.getTime()),
+                valor,
+                cpf,
+                cartao,
+                hora,
+                donoDaLoja,
+                nomeDaLoja
+        );
+    }
+
+    public Transacao wihtHora(String hora) throws ParseException {
+        var dateFormat = new SimpleDateFormat("HHmmss");
+        var date = dateFormat.parse(hora);
+
+        return new Transacao(
+                id,
+                tipo,
+                data,
+                valor,
+                cpf,
+                cartao,
+                new Time(date.getTime()),
+                donoDaLoja,
+                nomeDaLoja
+        );
+    }
+}
---
+++ src/main/java/com/sergiolemos/backend/domain/TransacaoCNAB.java
@@ -0,0 +1,14 @@
+package com.sergiolemos.backend.domain;
+
+import java.math.BigDecimal;
+
+public record TransacaoCNAB(
+        Integer tipo,
+        String data,
+        BigDecimal valor,
+        Long cpf,
+        String cartao,
+        String hora,
+        String donoDaLoja,
+        String nomeDaLoja) {
+}
